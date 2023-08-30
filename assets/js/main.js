2b81cbef8aecc6b94041be4426e831510a336578
ilias <essatenilas@gmail.com>
2023-08-30 23:30:59 +0300

    init: header complated

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+let navIconsContainer = document.querySelector("#nav-icons")
+let navIcons = document.querySelector(".header-icons")
+navIconsContainer.appendChild(navIcons)
+navIcons.classList.remove("d-none")
